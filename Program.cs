d1a9d7d804e87952067cbe057126843ac5a90ee6
Emma Andersson <Emma@Emmas-MacBook-Air-2.local>
2025-05-11 22:25:12 +0200

    Using helper methods

--- Program.cs
+++ Program.cs
@@ -101,6 +101,42 @@ namespace BasicCSharp
                 Console.WriteLine(names[i]);
             }
             Console.ReadLine(); */
+
+            // Using helper methods
+            /* Console.WriteLine("The Name Game");
+
+            Console.Write("What's your first name: ");
+            string firstName = Console.ReadLine() ?? "";
+
+            Console.Write("What's your last name: ");
+            string lastName = Console.ReadLine() ?? "";
+
+            Console.Write("What city are you in? ");
+            string city = Console.ReadLine() ?? "";
+
+            DisplayResult(ReverseString(firstName), ReverseString(lastName), ReverseString(city));
+
+            Console.ReadLine();
+        }
+
+        private static string ReverseString(string message)
+        {
+            char[] msgArray = message.ToCharArray();
+            Array.Reverse(msgArray);
+            return String.Concat(msgArray);
+        }
+
+        private static void DisplayResult(string reversedFirstName, string reversedLastName, string reversedCity)
+        {
+            Console.Write("Results: ");
+            Console.Write(String.Format("{0} {1} {2}", reversedFirstName, reversedLastName, reversedCity));  // Corrected this line
+        }
+
+        private static void DisplayResult(string message)
+        {
+            Console.Write("Results: ");
+            Console.Write(message);
+        */
         }
     }
 }
